5155ff15c5f7199dcc0388790a0699fdf562fc88
led_min@126.com <led-min1102>
2018-12-13 09:06:23 +0800

    集成redis swagger

---
+++ col-kafka-consumer2/src/main/java/com/col/ColKafkaConsumer2Application.java
@@ -0,0 +1,12 @@
+package com.col;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColKafkaConsumer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColKafkaConsumer2Application.class, args);
+	}
+}
